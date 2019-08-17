50f1e9a99c9fd5bdd1ca986353994991bde4a635
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-17 20:05:12 +0530

    stl: tuple1: some tuple-foo

---
+++ stl/tuple1.cpp
@@ -0,0 +1,28 @@
+#include <complex>
+#include <iostream>
+#include <string>
+#include <tuple>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	tuple<string, int, int, complex<double>> t;
+	tuple<int, float, string> t1(41, 6.3, "mfrw");
+
+	cout << get<0>(t1) << " ";
+	cout << get<1>(t1) << " ";
+	cout << get<2>(t1) << " ";
+	cout << endl;
+
+	auto t2 = make_tuple(22, 44, "mfrw");
+
+	get<1>(t1) = get<1>(t2);
+
+	if (t1 < t2) {
+		t1 = t2;
+	}
+
+	cout << get<1>(t1) << endl;
+
+	return 0;
+}
