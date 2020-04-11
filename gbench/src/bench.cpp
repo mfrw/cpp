a434b6f100d1bff39868d6dfb957325c252ddd75
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-04-11 18:31:36 +0530

    gbench: benchmark memory access
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- gbench/src/bench.cpp
+++ gbench/src/bench.cpp
@@ -1,4 +1,5 @@
 #include "benchmark.h"
+#include "setbench.h"
 
 static void BM_StringCreation(benchmark::State& state) {
 	for (auto _ : state) std::string empty_string;
@@ -23,5 +24,28 @@ static void BM_FibRecursive(benchmark::State& state) {
 }
 // BENCHMARK(BM_FibRecursive)->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_FibRecursive);
+
+template <class Set>
+void bm_set(benchmark::State& state) {
+	std::default_random_engine engine = std::default_random_engine{0};
+	auto size = 1000;
+	Set set;
+	set.generate(engine, size);
+	for (auto _ : state) benchmark::DoNotOptimize(set.contains(engine()));
+}
+static void bencher(benchmark::State& state) { bm_set<std_set>(state); }
+
+BENCHMARK(bencher)->Range(8, 8 << 10);
+
+static void bm_memory_access(benchmark::State& state) {
+	auto bytes = state.range(0) * 1024;
+	std::vector<unsigned> data(bytes / sizeof(unsigned));
+	std::generate(data.begin(), data.end(), std::default_random_engine{});
+
+	for (auto _ : state)
+		for (auto& i : data) benchmark::DoNotOptimize(++i);
+	state.SetBytesProcessed(state.iterations() * data.size());
+}
+BENCHMARK(bm_memory_access)->Range(8, 8 << 10);
 
 BENCHMARK_MAIN();
